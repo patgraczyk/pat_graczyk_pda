73022e3db2d557a1990385ffd235c1d06f998182
Pat <graczyk.pat@gmail.com>
2018-10-24 22:12:21 +0100

    integration started

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -16,4 +16,8 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should update the display with the result of the arithmetical operation', function(){
+    running_total = element(by.css('#running_total'))
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
 });
